5c9cb0df0fccbe127c40799e5528b902b8c2b726
valeriamadaev <valeriamadaev@VALERIAMADAA045>
2018-11-05 19:04:13 +0200

    handle search also by destination

--- TakeAFlight/Controllers/HomeController.cs
+++ TakeAFlight/Controllers/HomeController.cs
@@ -5,33 +5,45 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using TakeAFlight.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace TakeAFlight.Controllers
 {
-	public class HomeController : Controller
-	{
-		public IActionResult Index()
-		{
-			return View();
-		}
+    public class HomeController : Controller
+    {
+        TakeAFlightContext dbContext;
+        public HomeController(TakeAFlightContext context)
+        {
+            dbContext = context;
+        }
 
-		public IActionResult About()
-		{
-			ViewData["Message"] = "Your application description page.";
+        public IActionResult Index()
+        {
+            ViewBag.Items = dbContext.Destinations.Select(obj => new SelectListItem()
+            {
+                Text = obj.ToString(),
+                Value = obj.DestinationID.ToString()
+            }).ToList();
+            return View();
+        }
 
-			return View();
-		}
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
 
-		public IActionResult Contact()
-		{
-			ViewData["Message"] = "Your contact page.";
+            return View();
+        }
 
-			return View();
-		}
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
 
-		public IActionResult Error()
-		{
-			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-		}
-	}
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
 }
