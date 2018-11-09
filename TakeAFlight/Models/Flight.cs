d7238a8633c73f7f05b3dfd3eddb36dca1df93a5
roei <roei@DESKTOP-QUB3G14>
2018-11-09 13:20:56 +0200

    adding some field check on search and flight controller

--- TakeAFlight/Models/Flight.cs
+++ TakeAFlight/Models/Flight.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +9,14 @@ namespace TakeAFlight.Models
 	public class Flight
 	{
 		public int FlightID { get; set; }
+		[Required]
 		public int DestinationID { get; set; }
+		[Required]
+		[RegularExpression("([1-9][0-9]*)", ErrorMessage = "Price Should be a postive number")]
 		public double Price { get; set; }
+		[Required]
 		public TimeSpan? Duration { get; set; }
+		[Required]
 		public DateTime? Departure { get; set; }
 
 		public virtual Destination Destination { get; set; }
